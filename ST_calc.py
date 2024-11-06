9f2810a5bf3ce174db2181ee00c1d3d4287c6829
Emilie Kibsgaard <em_kibsgaard@outlook.dk>
2024-11-06 01:30:04 +0100

    files add

---
+++ ST_calc.py
@@ -0,0 +1,237 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+from scipy.stats import spearmanr, kendalltau, linregress
+from rpy2 import robjects
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+import numpy as np
+import piflib 
+from piflib import compute_cigs
+import warnings
+
+
+
+# Suppress SettingWithCopyWarning
+warnings.simplefilter(action='ignore', category=pd.core.common.SettingWithCopyWarning)
+
+# Activate pandas <-> R DataFrame conversion
+pandas2ri.activate()
+from rpy2.robjects.conversion import localconverter
+
+
+# Import the sdcMicro package
+sdcMicro = importr('sdcMicro')
+
+
+
+def stats(suda, pif, suda_att, k_combined_field):
+    
+    
+    print('\n' + '='*40)
+    print('        ROW LEVEL CORRELATION      ')
+    print('='*40 + '\n')
+    
+    
+    # Spearman Rank Correlation
+    correlation_spearman, _ = spearmanr(suda['dis-score'], pif['RIG'])
+    print(f"Spearman Rank Correlation between suda & pif: {correlation_spearman:.2f}")  
+    
+    # Kendall's Tau Correlation
+    tau, p_value = kendalltau(suda['dis-score'], pif['RIG'])
+    print(f"Kendall's Tau between suda & pif: {tau:.2f}, p-value: {p_value:.4f}")
+    
+    # Pearson Correlation
+    correlation_pearson = suda['dis-score'].corr(pif['RIG'])
+    
+    #field relationship
+    
+    sum_df = pif.drop(columns=['RIG']).sum()
+    sum_df = pd.DataFrame(list(sum_df.items()), columns=['variable', 'sum'])
+    
+    
+    # Merge the two DataFrames on 'variable'
+    merged_field_values = pd.merge(suda_att, sum_df, on='variable')
+    merged_field_values = pd.merge(merged_field_values, k_combined_field, on='variable') 
+    
+
+
+    # Calculate the correlation between 'contribution' and 'Sum'
+    correlation_pif_suda = merged_field_values['contribution'].corr(merged_field_values['sum'])
+    correlation_pif_k = merged_field_values['Normalized Difference'].corr(merged_field_values['sum'])
+    correlation_suda_k = merged_field_values['contribution'].corr(merged_field_values['Normalized Difference'])
+
+    
+    
+    
+    
+    # Plot with trend line
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.scatter(suda['dis-score'], pif['RIG'], alpha=0.3, label='Data Points')
+    
+    # Fit and plot the trend line
+    slope, intercept, _, _, _ = linregress(suda['dis-score'], pif['RIG'])
+    ax.plot(suda['dis-score'], slope * suda['dis-score'] + intercept, color='red', label='Trend Line')
+    
+    ax.set_title(f'Pearson Correlation: {correlation_pearson:.2f}')
+    ax.set_xlabel('suda["dis-score"]')
+    ax.set_ylabel('pif["RIG"]')
+    ax.grid(False)
+    ax.legend()
+    
+    plt.show()    
+    plt.close(fig)
+    print('____________________________________________________________________________________')
+    
+    print('\n' + '='*40)
+    print('      FIELD LEVEL CORRELATIONS     ')
+    print('='*40 + '\n')
+    
+    result_text_field_s_p = f"Pearson Correlation between SUDA & PIF field level: {correlation_pif_suda:.2f}"
+    result_text_field_p_k = f"Pearson Correlation between K-combined & PIF field level: {correlation_pif_k:.2f}"
+    result_text_field_s_k = f"Pearson Correlation between SUDA & K-combined field level: {correlation_suda_k:.2f}"
+
+    print(result_text_field_s_p)
+    print(result_text_field_p_k)
+    print(result_text_field_s_k)
+    
+    print('\n')
+    
+    
+    # Spearman correlation between PIF & SUDA
+    correlation_spearman_pif_suda, _ = spearmanr(merged_field_values['contribution'], merged_field_values['sum'])
+    print(f"Spearman Rank Correlation between PIF & SUDA: {correlation_spearman_pif_suda:.2f}")
+
+    # Spearman correlation between PIF & K
+    correlation_spearman_pif_k, _ = spearmanr(merged_field_values['Normalized Difference'], merged_field_values['sum'])
+    print(f"Spearman Rank Correlation between PIF & K: {correlation_spearman_pif_k:.2f}")
+
+    # Spearman correlation between SUDA & K
+    correlation_spearman_suda_k, _ = spearmanr(merged_field_values['contribution'], merged_field_values['Normalized Difference'])
+    print(f"Spearman Rank Correlation between SUDA & K: {correlation_spearman_suda_k:.2f}")
+
+    return 
+
+
+
+
+def convert_to_numeric(df):
+    for col in df.select_dtypes(include=['object']).columns:
+        df[col] = df[col].astype('category').cat.codes
+    return df
+
+
+
+def compute_suda2(dataframe, sample_fraction=0.2, missing_value=np.NaN):
+    with localconverter(pandas2ri.converter):
+        r_df = robjects.conversion.py2rpy(dataframe)
+        
+    
+    convert_to_numeric(dataframe) 
+    
+    suda_result = sdcMicro.suda2(r_df, missing=missing_value, DisFraction=sample_fraction)
+    contribution_percent = list(suda_result.rx2('contributionPercent'))
+    score = list(suda_result.rx2('score'))
+    dis_score = list(suda_result.rx2('disScore'))
+
+    return (contribution_percent, score, dis_score,suda_result)
+
+
+
+
+
+import piflib.pif_calculator as pif
+def calculate_summed_dis_scores(k_combined_all, AOMIC, sample_fraction=0.3, missing_value= -999):
+
+    dis_scores = []
+    pif_score = []
+    
+    k_combined_all = k_combined_all[k_combined_all['Combination'].str.count(',') >= 2]
+    
+    for index, row in k_combined_all.iterrows():
+        combination_fields = row['Combination'].split(', ')
+       
+        df_subset = AOMIC[combination_fields]
+        df_subset = df_subset.fillna(-999)
+        
+        contribution_percent, score, dis_score, attribute_contributions = compute_suda2(
+            df_subset, sample_fraction=sample_fraction, missing_value=missing_value)  
+        
+        summed_dis_score = sum(dis_score)
+        dis_scores.append(summed_dis_score)
+        
+        cigs = piflib.compute_cigs(df_subset)
+        pif_95 = pif.compute_pif(cigs, 0.95)
+        pif_score.append(pif_95)
+        
+
+    k_combined_all['pif_score'] = pif_score
+    k_combined_all['suda_score'] = dis_scores
+    k_combined_all.fillna(0, inplace=True)
+    
+    return k_combined_all
+
+
+
+def plot_calc(k_combined_all): 
+    
+   
+    k_combined_all = k_combined_all.replace([np.inf, -np.inf], 0)
+
+    correlation, _ = spearmanr(k_combined_all['Score'], k_combined_all['suda_score'])
+    print(f"Spearman Correlation between Suda sum and K-combined: {correlation:.2f}")
+    
+    pearson_corr = k_combined_all['Score'].corr(k_combined_all['suda_score'])
+    print('Pearson correlation to Suda sum & K-combined',pearson_corr)
+    
+    print('________________________________________________________________________')
+    
+    print('Pearson Correlation:', pearson_corr)
+
+    
+    plt.figure(figsize=(8, 4))
+    plt.scatter(k_combined_all['Score'], k_combined_all['suda_score'], alpha=0.6)
+    plt.title(f'Scatter Plot of k-combined Score vs. SUDA Score\n(Pearson Correlation: {pearson_corr:.2f})')
+    plt.xlabel('K-combined Score')
+    plt.ylabel('SUDA Score')
+    plt.grid(False)
+
+    
+    plt.show()
+    
+    print('________________________________________________________________________')
+    print('\n')
+    
+    correlation, _ = spearmanr(k_combined_all['Score'], k_combined_all['pif_score'])
+    print(f"Spearman Correlation between PIF 95% and K-combined: {correlation:.2f}")
+    
+    pearson_corr = k_combined_all['Score'].corr(k_combined_all['pif_score'])
+    print('Pearson correlation to PIF 95% & K-combined',pearson_corr)
+    
+    plt.figure(figsize=(8, 4))
+    plt.scatter(k_combined_all['Score'], k_combined_all['pif_score'], alpha=0.6)
+    plt.title(f'Scatter Plot of k-combined Score vs. Pif score\n(Pearson Correlation: {pearson_corr:.2f})')
+    plt.xlabel('K-combined Score')
+    plt.ylabel('Pif score')
+    plt.grid(False)
+
+    
+    plt.show()
+    
+    print('________________________________________________________________________')
+    print('\n')
+    
+    correlation, _ = spearmanr(k_combined_all['suda_score'], k_combined_all['pif_score'])
+    print(f"Spearman Correlation between PIF 95% and SUDA: {correlation:.2f}")
+    
+    pearson_corr = k_combined_all['suda_score'].corr(k_combined_all['pif_score'])
+    print('Pearson correlation to PIF 95% & SUDA',pearson_corr)
+    
+    plt.figure(figsize=(8, 4))
+    plt.scatter(k_combined_all['suda_score'], k_combined_all['pif_score'], alpha=0.6)
+    plt.title(f'Scatter Plot of suda Score vs. Pif score\n(Pearson Correlation: {pearson_corr:.2f})')
+    plt.xlabel('suda Score')
+    plt.ylabel('Pif score')
+    plt.grid(False)
+        
+        
+    return 
